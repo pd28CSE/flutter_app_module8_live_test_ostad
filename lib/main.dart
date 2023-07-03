eca99cebd3f0fb240999a127b50cfe1e5f1cf000
Partho Debnath <parthodebnath28@gmail.com>
2023-07-03 22:51:12 +0600

    add flutter app basic structure

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Live Test',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App Bar'),
+      ),
+      body: const Center(
+        child: Text('Text'),
+      ),
+    );
+  }
+}
